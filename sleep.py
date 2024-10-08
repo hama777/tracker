97bfba59730ba5e607520484813ac008a5a82cfe
hama777 <GEB01604@nifty.ne.jp>
2024-10-08 18:57:06 +0900

    24/10/08 v1.28 古い処理を削除

--- sleep.py
+++ sleep.py
@@ -12,7 +12,8 @@ from datetime import date,timedelta
 import math
 import numpy as np
 
-version = "1.27"       # 24/10/04 v1.27 month_info_list 廃止
+# 24/10/08 v1.28 古い処理を削除
+version = "1.28"       
 
 # TODO:  pixela
 
@@ -29,7 +30,6 @@ logfile = appdir + "\\tracker.log"
 pastdata = appdir + "/sleeppast.txt"
 rawdata = appdir + "/rawdata.txt"
 past_pf_dic = []   #  過去の月別時間 pf   辞書  キー  hhmm   値  分
-#month_info_list = []   # 月ごとのリスト  要素は 年月 平均時間  平均就寝時刻  平均起床時刻
 
 ftp_host = ftp_user = ftp_pass = ftp_url =  ""
 df = ""
@@ -48,7 +48,6 @@ def main_proc():
     ftp_url = ftp_url.replace("index.htm","sleep.htm")
     read_data()
     read_pastdata()
-    #create_month_info()
     create_df_month()
     parse_template()
 
@@ -119,30 +118,8 @@ def daily_graph() :
     for index , row in df.tail(30).iterrows() :    
         str_date = index.strftime("%d")
         stime = int(row['sleep'])
-        #hh = int(stime / 60)
-        #mm = stime % 60
 
         out.write(f"['{str_date}',[{conv_time_to_graph_str(stime)}]],")
-        #out.write(f"['{str_date}',[{hh},{mm},0]],")
-
-#   月別平均睡眠時間グラフ
-def month_graph_old() :
-    for index , row in df_past.iterrows() :  
-        yy = int(row['yymm'].split("/")[0]) - 2000
-        mon = int(row['yymm'].split("/")[1]) 
-        hh = int(row['sleep_ave'].split(":")[0])
-        mm = int(row['sleep_ave'].split(":")[1])
-        tm = hh * 60 + mm 
-        out.write(f"['{yy}{mon:02}',[{hh},{mm},0]],")
-
-    for dt in month_info_list :    
-        yymm = dt[0]
-        #tm = dt[1]
-        #hh = int(tm) // 60
-        #mm = int(tm) % 60
-        yy = yymm.year - 2000
-        mon = yymm.month
-        out.write(f"['{yy}{mon:02}',[{conv_time_to_graph_str(dt[1])}]],")
 
 #   月別平均睡眠時間グラフ
 def month_graph() :
@@ -152,29 +129,6 @@ def month_graph() :
         hh = tm // 60
         mm = tm % 60
         out.write(f"['{date_str}',[{hh},{mm},0]],")
-
-#   月別平均就寝時刻グラフ
-def month_start_time_graph_old() :
-    for index , row in df_past.iterrows() :  
-        if type(row['start_ave']) is not str :
-            continue 
-        yy = int(row['yymm'].split("/")[0]) - 2000
-        mon = int(row['yymm'].split("/")[1]) 
-        hh = int(row['start_ave'].split(":")[0])
-        mm = int(row['start_ave'].split(":")[1])
-        if hh < 8 :
-            hh = hh + 24
-        tm = hh * 60 + mm 
-        out.write(f"['{yy}{mon:02}',[{hh},{mm},0]],")
-
-    for dt in month_info_list :
-        yymm = dt[0]
-        yy = yymm.year - 2000
-        mon = yymm.month
-        #start  = dt[4]        # 月平均就寝時刻
-        #hh  = start // 60 
-        #mm  = start % 60 
-        out.write(f"['{yy}{mon:02}',[{conv_time_to_graph_str(dt[4])}]],")   # dt[4] = 月平均就寝時刻
 
 #   月別平均就寝時刻グラフ
 def month_start_time_graph() :
@@ -189,27 +143,6 @@ def month_start_time_graph() :
         mm = tm % 60
         out.write(f"['{date_str}',[{hh},{mm},0]],")
 
-
-#   月別平均起床時刻グラフ
-def month_end_time_graph_old() :
-    for index , row in df_past.iterrows() :  
-        if type(row['end_ave']) is not str :
-            continue 
-        yy = int(row['yymm'].split("/")[0]) - 2000
-        mon = int(row['yymm'].split("/")[1]) 
-        hh = int(row['end_ave'].split(":")[0])
-        mm = int(row['end_ave'].split(":")[1])
-        out.write(f"['{yy}{mon:02}',[{hh},{mm},0]],")
-
-    for dt in month_info_list :
-        yymm = dt[0]
-        yy = yymm.year - 2000
-        mon = yymm.month
-        #start  = dt[7]             # 月平均起床時刻
-        #hh  = start // 60 
-        #mm  = start % 60 
-        out.write(f"['{yy}{mon:02}',[{conv_time_to_graph_str(dt[7])}]],")   # dt[7] 月平均起床時刻 
-
 #   月別平均起床時刻グラフ
 def month_end_time_graph() :
     for dt , row in df_month.iterrows() :  
@@ -220,7 +153,6 @@ def month_end_time_graph() :
         hh = tm // 60
         mm = tm % 60
         out.write(f"['{date_str}',[{hh},{mm},0]],")
-
 
 def start_time_graph() :
     for index , row in df.tail(90).iterrows() :    
@@ -289,104 +221,18 @@ def rank_month_sleep_min() :
         hhmm = conv_time_to_str(row['sleep_ave'])
         out.write(f"<tr><td align='right'>{i}</td><td align='right'>{hhmm}</td><td>{date_str}</td></tr>")
 
-
-# 月ごとの情報 month_info_list を作成する
-# month_info_list は月ごとのリスト  要素は 年月 平均時間  平均就寝時刻  平均起床時刻
-def create_month_info() :
-    global month_info_list
-    yymm_list = []
-    sleep_list = []
-    max_sleep = []
-    start_list = []
-    end_list = []
-    max_start = []
-    max_end = []
-    min_sleep = []
-    min_start = []
-    min_end = []
-    std_sleep = []
-    std_start = []
-    std_end = []
-    m_ave = df.resample(rule = "ME").mean().to_dict()
-    m_max = df.resample(rule = "ME").max().to_dict()
-    m_min = df.resample(rule = "ME").min().to_dict()
-    m_std = df.resample(rule = "ME").std().to_dict()
-    for d, tm in m_ave['sleep'].items():
-        yymm_list.append(d)
-        sleep_list.append(tm)
-    for _, tm in m_ave['start'].items():
-        start_list.append(tm)
-    for _, tm in m_ave['end'].items():
-        end_list.append(tm)
-    for _, tm in m_max['sleep'].items():
-        max_sleep.append(tm)
-    for _, tm in m_max['start'].items():
-        max_start.append(tm)
-    for _, tm in m_max['end'].items():
-        max_end.append(tm)
-    for _, tm in m_min['sleep'].items():
-        min_sleep.append(tm)
-    for _, tm in m_min['start'].items():
-        min_start.append(tm)
-    for _, tm in m_min['end'].items():
-        min_end.append(tm)
-    for _, tm in m_std['sleep'].items():
-        std_sleep.append(tm)
-    for _, tm in m_std['start'].items():
-        std_start.append(tm)
-    for _, tm in m_std['end'].items():
-        std_end.append(tm)
-
-    for yymm,sp,max_sp,min_sp,s,max_s,min_s,e,max_e,min_e,std_sp,std_s,std_e in zip(yymm_list,sleep_list,max_sleep,min_sleep,
-                                        start_list,max_start,min_start,
-                                        end_list,max_end,min_end,std_sleep,std_start,std_end  ) :
-        mlist = [yymm,sp,max_sp,min_sp,s,max_s,min_s,e,max_e,min_e,std_sp,std_s,std_e]
-        month_info_list.append(mlist)
-
-###############################################################################
 #  月の情報をdfで保持する
 #  df_month  カラム  yymm start_ave end_ave sleep_ave start_max end_max sleep_max start_min end_min sleep_min
 #            yymm は int  sleep 等は分単位 int 
 def create_df_month() :
     global  df_month
-    #m_ave = df.resample(rule = "ME").mean().to_dict()
+
     m_ave = df.resample(rule = "ME").mean()
     m_max = df.resample(rule = "ME").max()
     m_min = df.resample(rule = "ME").min()
     result = pd.concat([m_ave, m_max,m_min], axis=1)
     result.columns = ['start_ave','end_ave','sleep_ave','start_max','end_max','sleep_max','start_min','end_min','sleep_min']
     df_month = pd.concat([df_all,result ], axis=0)
-    #print(df_month)
-
-
-def month_info_table_old() :
-    for dt in month_info_list :
-        yymm = dt[0]
-        yy = yymm.year - 2000
-        mon = yymm.month
-        sleep  = int(dt[1])
-        max_sleep  = int(dt[2])
-        min_sleep  = int(dt[3])
-        start  = conv_time_to_str(dt[4])
-        max_start  = conv_time_to_str(dt[5])
-        min_start  = conv_time_to_str(dt[6])
-        end  = conv_time_to_str(dt[7])
-        max_end  = conv_time_to_str(dt[8])
-        min_end  = conv_time_to_str(dt[9])
-        std_sleep =  dt[10]
-        std_start =  dt[11]
-        std_end =  dt[12]
-        out.write(f'<tr><td>{yy}/{mon:02}</td><td  align="right">{sleep//60}:{sleep%60:02}</td>'
-                  f'<td  align="right">{std_sleep:5.2f}</td>'
-                  f'<td  align="right">{min_sleep//60}:{min_sleep%60:02}</td>'
-                  f'<td  align="right">{max_sleep//60}:{max_sleep%60:02}</td>'
-                  f'<td  align="right">{start}</td>'
-                  f'<td  align="right">{std_start:5.2f}</td>'
-                  f'<td>{min_start}</td><td>{max_start}</td>'
-                  f'<td  align="right">{end}</td>'
-                  f'<td  align="right">{std_end:5.2f}</td>'
-                  f'<td>{min_end}</td><td>{max_end}</td></tr>\n')
-    all_statistics()
 
 #  月別情報テーブル
 #  TODO: ループによる処理に変更
@@ -463,7 +309,6 @@ def date_settings():
     today_yy = today_date.year
     lastdate = today_date - timedelta(days=1)
 
-
 def conv_hhmm_mm(hhmm) :
     if hhmm == "" :
         return 0
@@ -474,7 +319,6 @@ def conv_hhmm_mm(hhmm) :
 def conv_yymm(yymm) :
     yy,mm = yymm.split("/")
     return int(yy) * 100 + int(mm)
-    
 
 def ftp_upload() : 
     if debug == 1 :
@@ -488,7 +332,6 @@ def today(s):
     s = s.replace("%today%",d)
     out.write(s)
     today = date.today()
-    
 
 def parse_template() :
     global out 
